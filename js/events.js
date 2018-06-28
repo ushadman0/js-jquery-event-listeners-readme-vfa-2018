7a60a3a786d34a65542ec28dc905cdd82731b816
Shadman Uddin <ushadman0@gmail.com>
2018-06-28 00:48:40 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,7 @@ function pressIt() {
 function submitIt() {
   $('form').on('submit', function(e) {
     alert('Your form is going to be submitted now.')
-  }
+  })
 }
 $(document).ready(function(){
 
